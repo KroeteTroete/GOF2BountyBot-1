94765e668fb48500e0e253aaee8decb7eaa4778a
Trimatix <1jasperlaw@gmail.com>
2021-01-23 23:17:07 +0000

    implement Weapon base class, convert primaryWeapon to a Weapon subclass, copy in turretWeapon

---
+++ bot/gameObjects/items/turretWeapon.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from .gameItem import spawnableItem
+from ...cfg import bbData
+from ... import lib
+from .weapon import Weapon
+
+
+@spawnableItem
+class TurretWeapon(Weapon):
+    """A turret that can be equipped onto a bbShip for use in duels.
+    """
+    
+    @classmethod
+    def fromDict(cls, turretDict : dict, **kwargs) -> TurretWeapon:
+        """Factory function constructing a new turretWeapon object from a dictionary serialised representation - the opposite of turretWeapon.toDict.
+        
+        :param dict turretDict: A dictionary containing all information needed to construct the desired turretWeapon
+        :return: A new turretWeapon object as described in turretDict
+        :rtype: turretWeapon
+        """
+        if turretDict["builtIn"]:
+            return bbData.builtInTurretObjs[turretDict["name"]]
+        else:
+            return TurretWeapon(turretDict["name"], turretDict["aliases"], dps=turretDict["dps"], value=turretDict["value"],
+                            wiki=turretDict["wiki"] if "wiki" in turretDict else "", manufacturer=turretDict["manufacturer"] if "manufacturer" in turretDict else "",
+                            icon=turretDict["icon"] if "icon" in turretDict else bbData.rocketIcon, emoji=lib.emojis.BasedEmojiFromStr(turretDict["emoji"]) if "emoji" in turretDict else lib.emojis.BasedEmoji.EMPTY,
+                            techLevel=turretDict["techLevel"] if "techLevel" in turretDict else -1, builtIn=False)
