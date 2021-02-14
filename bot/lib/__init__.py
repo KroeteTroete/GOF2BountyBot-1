f3bc8c03eff84f5e1737605e7420dc7b475cb7c0
Trimatix <1jasperlaw@gmail.com>
2021-02-14 15:44:06 +0000

    auto import pathfinding with lib

--- bot/lib/__init__.py
+++ bot/lib/__init__.py
@@ -1,2 +1,2 @@
 # Make all lib modules available on package import
-from . import discordUtil, emojis, jsonHandler, stringTyping, timeUtil, exceptions # noqa: F401
+from . import discordUtil, emojis, exceptions, jsonHandler, pathfinding, stringTyping, timeUtil # noqa: F401
