76ef9cb66f183f9fd7ecdd3b6eba1cd9c8419b76
Trimatix <1jasperlaw@gmail.com>
2021-03-14 00:02:28 +0000

    fix _sortGameObjects creating duplicate list references instead of new empty lists

--- bot/cfg/gameConfigurator.py
+++ bot/cfg/gameConfigurator.py
@@ -196,7 +196,7 @@ def _sortGameObjects(objsDB : Dict[str, Any]) -> List[List[Any]]:
     :rtype: List[List[Any]]
     """
     # Sort module objects by tech level
-    sortedDB = [[]] * (cfg.maxTechLevel - cfg.minTechLevel + 1)
+    sortedDB = [[] for _ in range(cfg.maxTechLevel - cfg.minTechLevel + 1)]
     for obj in objsDB.values():
         sortedDB[obj.techLevel - 1].append(obj)
     return sortedDB
