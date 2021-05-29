99ae3318ec3ac60fd304be75ee9f7091a8b236c3
Trimatix <1jasperlaw@gmail.com>
2021-05-30 00:01:54 +0100

    add a couple of docstrings and slightly faster itemPool validation

--- bot/gameObjects/items/tools/crateTool.py
+++ bot/gameObjects/items/tools/crateTool.py
@@ -1,4 +1,5 @@
 import random
+from typing import List
 from . import toolItem
 from .... import lib
 from discord import Message
@@ -6,27 +7,55 @@ from ....cfg import cfg
 from .. import gameItem
 from ....botState import logger
 from ....reactionMenus.confirmationReactionMenu import InlineConfirmationMenu
+from ....users.basedUser import BasedUser
 
 
 @gameItem.spawnableItem
 class CrateTool(toolItem.ToolItem):
-    def __init__(self, itemPool, name : str = "", value : int = 0, wiki : str = "",
+    """A tool containing a pool of GameItems which, when used, gives the user a single random item from the pool.
+    Also automatically removes itself from the user's inventory upon use.
+
+    :var itemPool: List of potential items to win. May contain duplicates.
+    :vartype itemPool: List[gameItem.GameItem]
+    """
+    def __init__(self, itemPool: List[gameItem.GameItem], name : str = "", value : int = 0, wiki : str = "",
             manufacturer : str = "", icon : str = "", emoji : lib.emojis.BasedEmoji = lib.emojis.BasedEmoji.EMPTY,
             techLevel : int = -1, builtIn : bool = False):
+        """
+        :param List[gameItem.GameItem] itemPool: List of potential items to win. May contain duplicates.
+        :param str name: The name of the crate. Must be unique.
+        :param int value: The number of credits that this item can be bought/sold for at a shop. (Default 0)
+        :param str wiki: A web page that is displayed as the wiki page for this item. (Default "")
+        :param str manufacturer: The name of the manufacturer of this item (Default "")
+        :param str icon: A URL pointing to an image to use for this item's icon (Default "")
+        :param lib.emojis.BasedEmoji emoji: The emoji to use for this item's small icon (Default lib.emojis.BasedEmoji.EMPTY)
+        :param int techLevel: A rating from 1 to 10 of this item's technical advancement, generally for crates this isn't
+                                limited, e.g a measure of the rarity of the items, or of the items' TLs maybe (Default -1)
+        :param bool builtIn: Whether this is a BountyBot standard crate (loaded in from JSON) or a custom spawned
+                                item (Default False)
+        """
 
         super().__init__(name, [], value=value, wiki=wiki,
             manufacturer=manufacturer, icon=icon, emoji=emoji,
             techLevel=techLevel, builtIn=builtIn)
 
-        for item in itemPool:
-            if not gameItem.isSpawnableItemInstance(item):
-                raise RuntimeError("Attempted to create a crateTool with something other than a spawnableItem " \
-                                    + "in its itemPool.")
-        self.itemPool = itemPool
+        try:
+            item = next(i for i in itemPool if not gameItem.isSpawnableItemInstance(i))
+        except StopIteration:
+            self.itemPool = itemPool
+        else:
+            logger.log("CrateTool", "__init__",
+                        "Attempted to create a crateTool with something other than a spawnableItem in its itemPool: " \
+                            + str(item), eventType="WRONG_TYPE", noPrint=True)
+            raise RuntimeError("Attempted to create a crateTool with something other than a spawnableItem " \
+                                + "in its itemPool: " + str(item))
 
 
     async def use(self, *args, **kwargs):
-        """This item's behaviour function. Intended to be very generic at this level of implementation.
+        """Behaviour function which adds a random item from the pool and adds it to the owner's inventory,
+        then removes the crate from their inventory. For use in a command, use userFriendlyUse
+
+        :param BasedUser callingBUser: The user who owns the crate
         """
         if "callingBUser" not in kwargs:
             raise NameError("Required kwarg not given: callingBUser")
