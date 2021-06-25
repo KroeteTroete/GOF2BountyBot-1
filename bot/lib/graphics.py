0f00240c222fc210d6f3625ab37ae76ce5fcc5c6
Trimatix <1jasperlaw@gmail.com>
2021-06-25 21:53:08 +0100

    default to single bit images

--- bot/lib/graphics.py
+++ bot/lib/graphics.py
@@ -1,15 +1,15 @@
 from PIL import Image, ImageDraw
 
-def progressBar(w: int, h: int, progress: float, mode: str = "L", bgColour: int = 0x000000, barColour: int = 0xffffff) -> Image.Image:
+def progressBar(w: int, h: int, progress: float, mode: str = "1", bgColour: int = 0, barColour: int = 1) -> Image.Image:
     """Create a simple progress bar with a black background. Background fills the image, not limited to the bar shape.
-    The default image mode is "L" - intended to be used for creating image masks.
+    The default image mode is "1" - single-bit images intended to be used for creating image masks.
     Changes to this should be reflected in bgColour and colour
 
     Adapted from https://www.programmersought.com/article/71441110786/
 
     :param int w: The image width
     :param int h: The image height
-    :param str mode: The image mode (Default L)
+    :param str mode: The image mode (Default 1)
     :param float progress: Percentage progress of the bar between 0 and 1
     :param int bgColour: Colour for the background of the whole image (default black)
     :param int colour: Colour to fill the bar with (default white)
