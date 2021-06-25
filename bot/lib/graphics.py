4cacf0ee75c6b5e2257c26feba09a7960e5069be
Trimatix <1jasperlaw@gmail.com>
2021-06-25 20:24:45 +0100

    implement progressBar drawing into lib.progressBar instead of lib.graphics

---
+++ bot/lib/graphics.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageDraw
+
+def progressBar(w: int, h: int, progress: float, mode: str = "L", bgColour: int = 0x000000, barColour: int = 0xffffff) -> Image.Image:
+    """Create a simple progress bar with a black background. Background fills the image, not limited to the bar shape.
+    The default image mode is "L" - intended to be used for creating image masks.
+    Changes to this should be reflected in bgColour and colour
+
+    Adapted from https://www.programmersought.com/article/71441110786/
+
+    :param int w: The image width
+    :param int h: The image height
+    :param str mode: The image mode (Default L)
+    :param float progress: Percentage progress of the bar between 0 and 1
+    :param int bgColour: Colour for the background of the whole image (default black)
+    :param int colour: Colour to fill the bar with (default white)
+    """
+    im = Image.new(mode, (w, h), bgColour)
+    drawObject = ImageDraw.Draw(im)
+    w = w * min(1, max(0.01, progress))
+
+    drawObject.ellipse(  ((0, 0),     (h, h)),         fill=barColour)
+    drawObject.ellipse(  ((w - h, 0), (w, h)),         fill=barColour)
+    drawObject.rectangle(((h / 2, 0), (w - h / 2, h)), fill=barColour)
+
+    return im
+
+
