bc0727cb6c568c4cbbe843323d7f187898b21902
Trimatix <1jasperlaw@gmail.com>
2021-06-29 12:36:47 +0100

    add cmd_loma_buy

--- bot/commands/usr_loma.py
+++ bot/commands/usr_loma.py
@@ -12,6 +12,78 @@ from ..gameObjects.inventories.inventoryListing import DiscountableItemListing
 
 bbCommands.addHelpSection(0, "loma")
 
+async def cmd_loma_buy(message : discord.Message, args : str, isDM : bool):
+    """Buy the item of the given item type, at the given index, from the user's loma shop.
+    The typical "sell" and "transfer" shortcut args from cmd_buy are not compatible with this command.
+
+    :param discord.Message message: the discord message calling the command
+    :param str args: string containing an item type and an index number
+    :param bool isDM: Whether or not the command is being called from a DM channel
+    """
+    if not botState.usersDB.idExists(message.author.id):
+        await message.channel.send("The Loma pirates do not have any items to sell!")
+        return
+
+    requestedBUser: BasedUser = botState.usersDB.getUser(message.author.id)
+    if requestedBUser.loma is None or requestedBUser.loma.isEmpty():
+        await message.channel.send("The Loma pirates do not have any items to sell!")
+        return
+
+    commandPrefix = cfg.defaultCommandPrefix if isDM else botState.guildsDB.getGuild(message.guild.id).commandPrefix
+
+    argsSplit = args.split(" ")
+    if len(argsSplit) < 2:
+        await message.channel.send(":x: Not enough arguments! Please provide both an item type (ship/weapon/module/turret) " \
+                                    + f"and an item number from `{commandPrefix}loma`")
+        return
+    if len(argsSplit) > 4:
+        await message.channel.send(":x: Too many arguments! Please only give an item type (ship/weapon/module/turret) " \
+                                    + f"and an item number from `{commandPrefix}loma`")
+        return
+
+    itemCategory = argsSplit[0].rstrip("s")
+    if itemCategory == "all" or itemCategory not in cfg.validItemNames:
+        await message.channel.send(":x: Invalid item name! Please choose from: ship, weapon, module, turret or tool.")
+        return
+
+    itemNum = argsSplit[1]
+    if not lib.stringTyping.isInt(itemNum):
+        await message.channel.send(":x: Invalid item number!")
+        return
+
+    itemNum = int(itemNum)
+    shopItemStock = requestedBUser.loma.getStockByName(itemCategory)
+    if itemNum > shopItemStock.numKeys:
+        if shopItemStock.numKeys == 0:
+            await message.channel.send(":x: The Loma pirates don't have any " + itemCategory + "s in stock!")
+        else:
+            await message.channel.send(":x: Invalid item number! Loma currently has " + str(shopItemStock.numKeys) \
+                                        + " " + itemCategory + "(s).")
+        return
+
+    if itemNum < 1:
+        await message.channel.send(":x: Invalid item number! Must be at least 1.")
+        return
+
+    requestedItem = shopItemStock[itemNum - 1].item
+
+    if not requestedBUser.loma.userCanAffordItemObj(requestedBUser, requestedItem):
+        await message.channel.send(":x: You can't afford that item! (" + str(requestedItem.value) + ")")
+        return
+
+    requestedBUser.credits -= requestedItem.value
+    requestedBUser.getInactivesByName(itemCategory).addItem(requestedItem)
+    shopItemStock.removeItem(requestedItem)
+
+    await message.channel.send(":moneybag: Congratulations on your new **" + requestedItem.name \
+                                + "**! \n\nYour balance is now: **" + str(requestedBUser.credits) + " credits**.")
+
+bbCommands.register("loma buy", cmd_loma_buy, 0, allowDM=False, helpSection="loma",
+                        signatureStr="**loma buy <item-type> <item-number>**",
+                        shortHelp="Buy the requested item from the pirates at Loma. Item numbers can be seen in `loma`.",
+                        longHelp="Buy the requested item from the pirates at Loma. Item numbers are shown next to items in " \
+                                    + "`loma`. The `sell` and `transfer` options from `shop buy` do not apply to loma.")
+
 
 async def cmd_loma(message : discord.Message, args : str, isDM : bool):
     """list the items currently available in the user's loma shop.
@@ -125,7 +197,7 @@ async def cmd_loma(message : discord.Message, args : str, isDM : bool):
     if sendDM:
         await message.add_reaction(cfg.defaultEmojis.dmSent.sendable)
 
-bbCommands.register("loma", cmd_loma, 0, allowDM=True, helpSection="loma shop", signatureStr="**loma** *[item-type]*",
+bbCommands.register("loma", cmd_loma, 0, allowDM=True, helpSection="loma", signatureStr="**loma** *[item-type]*",
                     shortHelp="List all items currently on offer, to you only, by the pirates at Loma.",
                     longHelp="List all items currently on offer, to you only, by the pirates at Loma. Give an item type " \
                         + "(ship/weapon/turret/module/tool) to only list items of that type.")
