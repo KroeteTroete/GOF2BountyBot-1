e13f78840664fa02eedc832517cfedcc988dc685
Trimatix <1jasperlaw@gmail.com>
2021-09-18 18:34:36 +0100

    implement git get

--- bot/commands/usr_github.py
+++ bot/commands/usr_github.py
@@ -95,6 +95,53 @@ botCommands.register("issue search", cmd_issue_search, 0, forceKeepArgsCasing=Tr
                         shortHelp="Search for GitHub issues with the given name, getting the 3 most similar issues.")
 
 
+async def cmd_issue_get(message : discord.Message, args : str, isDM : bool):
+    """Get the GitHub issue with the given number.
+
+    :param discord.Message message: the discord message calling the command
+    :param str args: string containing an issue number to fetch
+    :param bool isDM: Whether or not the command is being called from a DM channel
+    """
+    if not args:
+        await message.reply(":x: Please give an issue number to look up!")
+        return
+    
+    if not lib.stringTyping.isInt(args):
+        await message.reply(":x: That's not a number!")
+        return
+
+    issueNum = int(args)
+    if issueNum < 1:
+        await message.reply(":x: Your issue number must be at least 1!")
+        return
+    
+    await lib.discordUtil.startLongProcess(message)
+    issue: Issue = await getIssueByNumber(issueNum)
+
+    if issue is None:
+        await message.reply(":x: Unknown issue number!")
+        return
+
+    labelsStr = ', '.join(cfg.githubLabelNames.get(x.name, x.name) for x in issue.labels)
+    resultsEmbed = discord.Embed(title="GitHub Issue Lookup",
+                                description="__" + ('🟢' if issue.state == 'open' else '🔴') \
+                                            + f" [#{issue.number} {issue.title}]({issue.url})__\n" \
+                                            + (f"> `{labelsStr}`\n" if labelsStr else "")
+                                            + f"\n{issue.body}",
+                                colour=discord.colour.Colour.random())
+    prefix = cfg.defaultCommandPrefix if isDM else botState.guildsDB.getGuild(message.guild.id).commandPrefix
+    resultsEmbed.set_footer(text=f"GitHub repository linked in `{prefix}source`")
+    resultsEmbed.set_thumbnail(url=botState.client.user.avatar_url_as(size=64))
+
+    await message.reply(embed=resultsEmbed)
+    await lib.discordUtil.endLongProcess(message)
+
+botCommands.register("issue get", cmd_issue_get, 0, forceKeepArgsCasing=True, allowDM=True,
+                        aliases=["bug get", "issues get", "git get", "github get", "feature get"],
+                        helpSection="github", signatureStr="**issue get <issue-number>**",
+                        shortHelp="Get the GitHub issue with the given number.")
+
+
 async def cmd_issue(message : discord.Message, args : str, isDM : bool):
     """Various github issues actions
 
@@ -110,7 +157,8 @@ async def cmd_issue(message : discord.Message, args : str, isDM : bool):
 
     argsSplit = args.split(" ")
 
-    issueCmds = {"search": cmd_issue_search}
+    issueCmds = {"search": cmd_issue_search,
+                "get": cmd_issue_get}
     
     if argsSplit[0] in issueCmds:
         await issueCmds[argsSplit[0]](message, args[len(argsSplit[0])+1:], isDM)
